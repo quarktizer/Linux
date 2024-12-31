13360e4ee1d6f2e182d183b92ae159d0a4cbc5f5
quarktizer <quarktize@proton.me>
2024-12-31 00:13:10 -0600

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Enable UFW firewall
+echo "Enabling UFW firewall..."
+sudo ufw enable
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
 
